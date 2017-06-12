73fb8aa4bdd54602441307ceb51cea0b7b011eb3
saksham0037 <saksham.bhalla5@gmail.com>
2017-06-12 16:31:45 +0530

    string library functions

---
+++ code/string.lua
@@ -0,0 +1,9 @@
+local string1 = "Hello how are you?"
+a, b = string.find(string1, "are")
+print(a,b)
+
+print("This is".." a ".."beautiful day")
+
+print(string.len(string1))
+    
+print(string.reverse(string1))
